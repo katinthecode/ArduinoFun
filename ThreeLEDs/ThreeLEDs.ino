d788fa7e28c0a22b7c08662155961fba3e15c690
Katie Antlitz <katinthecode@gmail.com>
2024-09-22 12:36:00 -0500

    Update ThreeLEDs.ino
    updating more variables

--- ThreeLEDs/ThreeLEDs.ino
+++ ThreeLEDs/ThreeLEDs.ino
@@ -1,43 +1,50 @@
 //Using 3 LEDs
-//Blink red 5x
-//Blink green 10x
-//Blink blue 15x
+//Blink redPin 5x
+//Blink greenPin 10x
+//Blink bluePin 15x
 
-int RED = 13;
-int GREEN = 12;
-int BLUE = 11;
+int redPin = 13;
+int redCount = 5;
+
+int greenPin = 12;
+int greenCount = 10;
+
+int bluePin = 11;
+int blueCount = 15;
+
+int delayCount = 500;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  for (int i = 0; i < 5; i++){
-    digitalWrite(RED, HIGH);
-    delay(500);
+  for (int i = 0; i < redCount; i++){
+    digitalWrite(redPin, HIGH);
+    delay(delayCount);
 
-    digitalWrite(RED, LOW);
-    delay(500);
+    digitalWrite(redPin, LOW);
+    delay(delayCount);
   }
     
-  for (int i = 0; i < 10; i++){
-    digitalWrite(GREEN, HIGH);
-    delay(500);
+  for (int i = 0; i < greenCount; i++){
+    digitalWrite(greenPin, HIGH);
+    delay(delayCount);
 
-    digitalWrite(GREEN, LOW);
-    delay(500);
+    digitalWrite(greenPin, LOW);
+    delay(delayCount);
   }
   
-  for (int i = 0; i < 15; i++){
-    digitalWrite(BLUE, HIGH);
-    delay(500);
+  for (int i = 0; i < blueCount; i++){
+    digitalWrite(bluePin, HIGH);
+    delay(delayCount);
 
-    digitalWrite(BLUE, LOW);
-    delay(500);
+    digitalWrite(bluePin, LOW);
+    delay(delayCount);
   }
 }
